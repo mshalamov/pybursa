48460d3d9aafaf426fe30ed72ba661b014f5083d
Maksym Shalamov <m_shalamov@wargaming.net>
2014-12-07 18:53:00 +0200

    Minor fix

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -5,14 +5,12 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('',
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^$',
-        TemplateView.as_view(template_name='index.html'),
-        name='home'),
-    url(r'^persons$',
-        TemplateView.as_view(template_name='persons.html'),
-        name='persons'),
-    url(r'^person$',
-        TemplateView.as_view(template_name='person.html'),
-        name='home')
-)
+                       url(r'^$',
+                           TemplateView.as_view(template_name='index.html'),
+                           name='home'),
+                       url(r'^persons$',
+                           TemplateView.as_view(template_name='persons.html'),
+                           name='persons'),
+                       url(r'^person$',
+                           TemplateView.as_view(template_name='person.html'),
+                           name='home'))
